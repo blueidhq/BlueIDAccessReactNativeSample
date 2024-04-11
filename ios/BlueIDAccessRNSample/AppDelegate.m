a4bf0bc994cd30a433c3760b176d0a4cf929d51c
Luan Träsel <luantrasel77@gmail.com>
2024-04-11 10:21:14 -0300

    Cleanup + Restored some default configurations on podfile

--- ios/BlueIDAccessRNSample/AppDelegate.m
+++ ios/BlueIDAccessRNSample/AppDelegate.m
@@ -2,6 +2,7 @@
 
 #import <React/RCTBundleURLProvider.h>
 
+// Add this line to import blueid library
 @import blueid_access_react_native;
 
 @implementation AppDelegate
@@ -13,23 +14,37 @@
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
   
+  // Add this so that the SDK initializes once your app starts
   [BlueAppDelegate didFinishLaunchingWithOptions:launchOptions];
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+  // Add this so that the SDK clear timers and resources
+  // when app is about to become inactive
+  [BlueAppDelegate willResignActive];
+}
+
 - (void)applicationWillTerminate:(UIApplication *)application
 {
+  // Add this so that the SDK clear timers and resources
+  // when app is terminated
   [BlueAppDelegate willTerminate];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
+  // Add this so that the SDK restart timers and synchronize credentials
+  // when app has become active again
   [BlueAppDelegate didBecomeActive];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
+  // Add this so that the SDK clear timers and resources
+  // when app goes to background
   [BlueAppDelegate didEnterBackground];
 }
 
@@ -48,49 +63,3 @@
 }
 
 @end
-
-// #import "AppDelegate.h"
-
-// #import <React/RCTBundleURLProvider.h>
-
-// @import blueid_access_react_native;
-
-// @implementation AppDelegate
-
-// - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-// {
-//   self.moduleName = @"BlueIDAccessExample";
-//   // You can add your custom initial props in the dictionary below.
-//   // They will be passed down to the ViewController used by React Native.
-//   self.initialProps = @{};
-  
-//   [BlueAppDelegate didFinishLaunchingWithOptions:launchOptions];
-
-//   return [super application:application didFinishLaunchingWithOptions:launchOptions];
-// }
-
-// - (void)applicationWillTerminate:(UIApplication *)application
-// {
-//   [BlueAppDelegate willTerminate];
-// }
-
-// - (void)applicationDidBecomeActive:(UIApplication *)application
-// {
-//   [BlueAppDelegate didBecomeActive];
-// }
-
-// - (void)applicationDidEnterBackground:(UIApplication *)application
-// {
-//   [BlueAppDelegate didEnterBackground];
-// }
-
-// - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
-// {
-// #if DEBUG
-//   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
-// #else
-//   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-// #endif
-// }
-
-// @end
