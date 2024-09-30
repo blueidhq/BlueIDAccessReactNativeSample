d8a787b9eb1c00c6d4b0fff4ddaec7cd8358cdbb
Waqas Akram <waqas.akram.extern@blueid.net>
2024-09-30 15:02:37 +0500

    Updated dependencies and fixed build

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
     presets: ['module:@react-native/babel-preset'],
+    overrides: [{
+        "plugins": [
+          ["@babel/plugin-transform-private-methods", {
+          "loose": true
+        }]
+          ]
+         }],
 }
